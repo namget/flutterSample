d7469004211bbdbdf65b9ec111ed487c08eed043
yeboykr <yeboykr@bodyfriend.co.kr>
2019-11-15 14:05:02 +0900

    - 더하기 빼기

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # flutterSample
+
 flutter sample
+
+https://www.youtube.com/watch?v=ujVR-V8X6sQ
+노마드형 영상 따라하기
 sample code 
+
+
+
+android studio ## hot reload
+혹시 모르니 재부팅 and ctrl + s
+[https://stackoverflow.com/questions/50025217/flutter-hot-reload-in-android-studio](https://stackoverflow.com/questions/50025217/flutter-hot-reload-in-android-studio)
--- lib/main.dart
+++ lib/main.dart
@@ -7,92 +7,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(title: 'Flutter Demo Home Page3'),
+      home: StupidState(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class StupidState extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Counter createState() => Counter();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Counter extends State<StupidState> {
+  int count = 0;
 
-  void _incrementCounter() {
+  void add() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      count += 1;
+    });
+  }
+
+  void minus() {
+    setState(() {
+      count -= 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //어플리케이션의 메인파트 여기서 모든 옵션을 볼 수 있음
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        // title과 +,- 버튼 두개 필요한것은 column
+        // column 안에 children, children은 위젯의 배열
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            "the number is",
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        //마진과 패딩 등등 다 위젯이다
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 15),
+          child: Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 28),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            RaisedButton(child: Text("Add"), onPressed: add),
+            RaisedButton(child: Text("Minus"), onPressed: minus)
+          ],
+        )
+      ],
+    ));
   }
 }
